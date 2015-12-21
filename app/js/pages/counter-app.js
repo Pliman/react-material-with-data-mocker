f2ed89bbb5c7fc5015e9b12bbbc0983f89de5d80
Pliman <bin.sun@diaorong.com>
2015-12-21 11:09:10 +0800

    remove annotation

--- app/js/pages/counter-app.js
+++ app/js/pages/counter-app.js
@@ -1,4 +1,3 @@
-//////////////
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 import Counter from './counter/counter-component'
@@ -15,19 +14,3 @@ function mapDispatchToProps(dispatch) {
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Counter)
-//////////////
-
-//const React = require('react');
-//
-//const Page1 = React.createClass({
-//  render: function() {
-//    return (
-//      <div className="am-padding-vertical-lg">
-//        <h2>页面 1</h2>
-//        <p>页面内容</p>
-//      </div>
-//    );
-//  }
-//});
-//
-//module.exports = Page1;
