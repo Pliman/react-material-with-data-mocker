390a21f8a70ad1fe2e415c7fdc60ac517ad9172d
Pliman <bin.sun@diaorong.com>
2016-02-04 15:10:56 +0800

    refactor name

--- test/data-mocker/app.js
+++ test/data-mocker/app.js
@@ -1,6 +1,6 @@
 var koa = require('koa'),
 	route = require('koa-route'),
-	static = require('koa-static'),
+	koaStatic = require('koa-static'),
 	gzip = require('koa-gzip'),
 	bodyParser = require('koa-bodyparser');
 
@@ -8,8 +8,8 @@ var app = koa();
 
 app.use(gzip());
 // mock服务器可能在应用服务器没有启动的情况下启动，所以需要支持应用的静态文件
-app.use(static('../../dist'));
-app.use(static('mocks'));
+app.use(koaStatic('../../dist'));
+app.use(koaStatic('mocks'));
 app.use(bodyParser());
 
 app.use(route.get('/hello', function* () {
